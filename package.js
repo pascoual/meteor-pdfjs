7318e8e779008b5c6c77c31acd055e65f58e6877
Pascoual <pascoualito@gmail.com>
2015-02-10 00:55:45 +0100

    Change: PDFJS version to 1.0.68

--- package.js
+++ package.js
@@ -1,10 +1,14 @@
 Package.describe({
-  summary: "PDF rendering for your Meteor application (client only)"
+  name: "pascoual:pdfjs",
+  summary: "PDF rendering for your Meteor application (client only)",
+  version: "1.0.68",
+  git: "https://github.com/pascoual/meteor-pdfjs"
 });
 
 Package.on_use(function (api) {
-  api.add_files(['build/pdf.js'], 'client');
+  api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
+  api.add_files(['build/pdf.js', 'web/compatibility.js'], 'client');
   api.add_files(['build/pdf.worker.js',
-                 'web/compatibility.js'], 'client', {isAsset: true});
-  api.export && api.export('PDFJS'); 
+                'web/compatibility.js'], 'client', {isAsset: true});
+  api.export('PDFJS'); 
 });
